03c581eef4d5694b570979d61b59eed3eca2e70e
Andrii_Kozak1 <Andrii_Kozak1@epam.com>
2016-04-18 20:31:40 +0300

    Fixed by added wierd dependency recomended by github. <dependency>     <groupId>xml-apis</groupId>     <artifactId>xml-apis</artifactId>     <version>1.4.01</version> </dependency>

--- src/test/java/com/someone/pizzaservice/RepositoryTestConfig.java
+++ src/test/java/com/someone/pizzaservice/RepositoryTestConfig.java
@@ -15,11 +15,8 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
  * @author Andrii_Kozak1
  */
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations={"classpath:/appContext.xml"})
-//        {
-//    , 
-//    "classpath:/repoTestContext.xml"})
-//@ActiveProfiles("dev")
+@ContextConfiguration(locations={"classpath:/appTestContext.xml", "classpath:/repoTestContext.xml"})
+@ActiveProfiles("dev")
 public class RepositoryTestConfig {
     
 }
